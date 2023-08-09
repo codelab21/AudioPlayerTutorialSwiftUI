3df445fa0320c735d5228c751e9e9f3e969dcd09
codelab21 <codelab21@outlook.com>
2023-08-09 17:27:20 +0300

    Audio Player SwiftUI Codes added

--- AudioPlayerTutorial/ContentView.swift
+++ AudioPlayerTutorial/ContentView.swift
@@ -6,16 +6,96 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    let audioFileName = "bird"
+    
+    @State private var player: AVAudioPlayer?
+    
+    @State private var isPlaying = false
+    @State private var totalTime: TimeInterval = 0.0
+    @State private var currentTime: TimeInterval = 0.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            
+            VStack(spacing: 20) {
+                Text("Bird Sound")
+                    .font(.title)
+                
+                VStack(spacing: 20){
+                    Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                        .font(.largeTitle)
+                        .onTapGesture {
+                            isPlaying ? stopAudio() : playAudio()
+                        }
+                    Slider(value: Binding(get: {
+                       currentTime
+                   }, set: { newValue in
+                       seekAudio(to: newValue)
+                   }), in: 0...totalTime)
+                   .accentColor(.white)
+                    HStack {
+                       Text(timeString(time: currentTime))
+                           .foregroundColor(.white)
+                       Spacer()
+                       Text(timeString(time: totalTime))
+                           .foregroundColor(.white)
+                   }
+                   .padding(.horizontal)
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+                .cornerRadius(20)
+            }
+            .foregroundColor(.white)
         }
-        .padding()
+        .onAppear(perform: setupAudio)
+        .onReceive(Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()) { _ in
+            updateProgress()
+        }
+    }
+    
+    private func setupAudio() {
+        guard let url = Bundle.main.url(forResource: audioFileName, withExtension: "mp3") else {
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.prepareToPlay()
+            totalTime = player?.duration ?? 0.0
+        } catch {
+            print("Error loading audio: \(error)")
+        }
+    }
+    
+    private func playAudio(){
+        player?.play()
+        isPlaying = true
+    }
+    
+    private func stopAudio(){
+        player?.pause()
+        isPlaying = false
+    }
+    
+    private func updateProgress() {
+       guard let player = player else { return }
+       currentTime = player.currentTime
+   }
+    
+    private func seekAudio(to time: TimeInterval) {
+        player?.currentTime = time
+    }
+    
+    private func timeString(time: TimeInterval) -> String {
+        let minutes = Int(time) / 60
+        let seconds = Int(time) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
 
